f58c3c782542135282ceadb18e052f7d2b6ef19f
Tribyna <nikshutkov@mai.ru>
2025-10-17 12:35:55 +1000

    КТ2

---
+++ КТ2/КТ2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ2
+
+{
+    internal class Program
+    {
+        static void Main(string[] args) //КТ2
+        {
+            Book b = new Book();
+            b.Title = "Преступление и наказание";
+            b.Pages = 500;
+            b.PrintInfo();
+            Console.ReadKey();
+        }
+
+        public class Book
+        {
+            public string Title;
+            private int _pages;
+
+            public int Pages
+            {
+                get { return _pages; }
+                set { if (value >= 0) _pages = value; }
+
+            }
+            public void PrintInfo()
+            {
+                Console.WriteLine($"Книга: {Title}, страниц: {Pages}");
+            }
+
+        }
+    }
+}
