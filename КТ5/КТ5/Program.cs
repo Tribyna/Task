4f548a64c52ac1fd4c04dd8f8ab230d42223c83d
Tribyna <nikshutkov@mai.ru>
2025-10-22 14:00:17 +1000

    UPDATE

---
+++ КТ5/КТ5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace КТ5
+{
+    class Gun
+    {
+        public int Nshot;
+        public bool shot {  get; privet set; }
+
+        public void shotgun(string shot)
+        {
+            if (!shot)
+            {
+                shot = false;
+                Console.WriteLine("Выстрел!");
+            }
+            else 
+            {
+                Console.WriteLine("Разряжен");
+            }
+        }
+
+        public void reloud(bool shot; int Nshot) 
+        { 
+            shot = true;
+       
+        }
+    }
+
+    class Program
+    {
+        static void main(string[] args)
+        {
+            Gun gun = new Gun();
+        gun.shotgun();
+        }
+    }
+}
